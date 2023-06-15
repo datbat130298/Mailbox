8dfec2cd52255d1c332177a74e2afa5ee42d1bb8
DESKTOP-L3RF6MG\ENGINEER <TECHUP100>
2023-06-15 18:28:04 +0700

    SEM-1456: LabelPopup

--- src/features/Common/Components/ComposePopup/Components/ComposePopupButtonMore/ComposePopupMoreOptionItem.tsx
+++ src/features/Common/Components/ComposePopup/Components/ComposePopupButtonMore/ComposePopupMoreOptionItem.tsx
@@ -1,6 +1,7 @@
-import { MouseEvent } from 'react';
+import { MouseEvent, useState } from 'react';
 import { FiCheck } from 'react-icons/fi';
 import { twMerge } from 'tailwind-merge';
+import LabelPopup from './LabelPopup';
 
 interface ItemOption {
   id: number;
@@ -21,6 +22,8 @@ const ComposePopupMoreOptionItem = ({
   className,
   isActiveFormat,
 }: ComposePopupMoreOptionItemProps) => {
+  const [isShowPopupLabel, setIsShowPopupLabel] = useState<boolean>(false);
+
   const handleClick = (e: MouseEvent<HTMLDivElement>) => {
     e.stopPropagation();
     onClick();
@@ -33,29 +36,39 @@ const ComposePopupMoreOptionItem = ({
     }
   };
 
+  const handleMoveSelect = () => {
+    if (item.id === 4) {
+      setIsShowPopupLabel(true);
+    }
+  };
   return (
-    <div
-      className={twMerge(
-        'flex items-center gap-1 px-2 py-1 text-sm text-slate-700  hover:bg-slate-200',
-        className,
-        item.id === 1 && 'border-b border-slate-200',
-        item.id === 6 && 'border-t border-slate-200',
-      )}
-      role="button"
-      tabIndex={0}
-      onClick={(e) => handleClick(e)}
-    >
+    <>
       <div
         className={twMerge(
-          'invisible px-2 text-black',
-          localStorage.getItem('defaultFullScreen') === 'true' && item.id === 1 && 'visible',
-          isActiveFormat && item.id === 2 && 'visible',
+          'relative flex items-center gap-1 px-2 py-1 text-sm  text-slate-700 hover:bg-slate-200',
+          className,
+          item.id === 1 && 'border-b border-slate-200',
+          item.id === 6 && 'border-t border-slate-200',
         )}
+        role="button"
+        tabIndex={0}
+        onClick={(e) => handleClick(e)}
+        onMouseMove={() => handleMoveSelect()}
+        onMouseLeave={() => setIsShowPopupLabel(false)}
       >
-        <FiCheck size={18} />
+        <div
+          className={twMerge(
+            'invisible px-2 text-black',
+            localStorage.getItem('defaultFullScreen') === 'true' && item.id === 1 && 'visible',
+            isActiveFormat && item.id === 2 && 'visible',
+          )}
+        >
+          <FiCheck size={18} />
+        </div>
+        <div className="pr-5 text-left">{item.label}</div>
       </div>
-      <div className="pr-5 text-left">{item.label}</div>
-    </div>
+      {isShowPopupLabel && <LabelPopup handleMoveSelect={handleMoveSelect} />}
+    </>
   );
 };
 
